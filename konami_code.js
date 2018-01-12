297f533b4fb736663030ffae02e6423aa0edc7e2
James Overbay <orangetoque13@gmail.com>
2018-01-12 01:36:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,19 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0
-function init() {
-  const key
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let index = 0;
+function init(e) {
+  const Down = parseInt(e.detail || e.which);
+  if (keyDown === code[index]) {
+    index++;
+    
+    if (index === code.length) {
+      alert("Congratulations, you have been awarded 30 lives!") ;
+      
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+
+let body = document.querySelector('body')
+body.addEventListener('keydown', init(e))
